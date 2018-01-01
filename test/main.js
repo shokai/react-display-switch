a1f6b19d483f7444d0fa7ea44bc30e860fdac1d2
Sho Hashimoto <hashimoto@shokai.org>
2018-01-01 23:57:45 +0900

    add simple condition case

--- test/main.js
+++ test/main.js
@@ -1,16 +1,36 @@
 /* eslint-env jest */
 
+import React from 'react'
+import renderer from 'react-test-renderer'
 import {When} from '../src/'
 
-describe('When component', function () {
-  it('should be a function', function () {
+describe('When component', () => {
+  it('should be a function', () => {
     expect(When).toBeInstanceOf(Function)
   })
 
-  describe('case', function () {
-    it('register conditions', function () {
-      When.case('admin-user', () => true)
-      expect(When.case('admin-user')).toBe(true)
+  describe('case', () => {
+    it('register conditions', () => {
+      When.case('login-user', () => true) // register
+      expect(When.case('login-user')).toBe(true)
+    })
+  })
+
+  describe('simple condition case', () => {
+    it('return children when match', () => {
+      const tree = renderer
+            .create(<When login-user><a href='./profile'>profile page</a></When>)
+            .toJSON()
+      expect(tree.type).toBe('a')
+      expect(tree.props.href).toBe('./profile')
+    })
+
+    it('return null when not match', () => {
+      When.case('admin-user', () => false)
+      const tree = renderer
+            .create(<When admin-user><a href='./settings'>settings page</a></When>)
+            .toJSON()
+      expect(tree).toBe(null)
     })
   })
 })
