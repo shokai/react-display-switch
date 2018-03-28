d66c96f4be8f4f212b0649b69d7ade64503c2693
Sho Hashimoto <hashimoto@shokai.org>
2018-03-28 10:57:16 +0900

    follow standard

--- test/not.js
+++ test/not.js
@@ -14,16 +14,16 @@ describe('WhenNot component', () => {
 
     it('return children when match', () => {
       const tree = renderer
-            .create(<WhenNot login_user><a href='/login'>login</a></WhenNot>)
-            .toJSON()
+        .create(<WhenNot login_user><a href='/login'>login</a></WhenNot>)
+        .toJSON()
       expect(tree.type).toBe('a')
       expect(tree.props.href).toBe('/login')
     })
 
     it('return null when not match', () => {
       const tree = renderer
-            .create(<WhenNot guest_user>hello guest</WhenNot>)
-            .toJSON()
+        .create(<WhenNot guest_user>hello guest</WhenNot>)
+        .toJSON()
       expect(tree).toBe(null)
     })
   })
@@ -35,15 +35,15 @@ describe('WhenNot component', () => {
       When.case('screen_md', () => windowWidth >= 768 && windowWidth < 992)
       When.case('screen_lg', () => windowWidth >= 992)
       const tree = renderer
-            .create(<WhenNot guest_user and screen_xs>hello guest</WhenNot>)
-            .toJSON()
+        .create(<WhenNot guest_user and screen_xs>hello guest</WhenNot>)
+        .toJSON()
       expect(tree).toBe('hello guest')
     })
 
     it('return null when not match', () => {
       const tree = renderer
-            .create(<WhenNot guest_user and screen_md>hello guest</WhenNot>)
-            .toJSON()
+        .create(<WhenNot guest_user and screen_md>hello guest</WhenNot>)
+        .toJSON()
       expect(tree).toBe(null)
     })
   })
@@ -51,15 +51,15 @@ describe('WhenNot component', () => {
   describe('"or" conditions', () => {
     it('return children when match', () => {
       const tree = renderer
-            .create(<WhenNot screen_xs or screen_lg>medium screen</WhenNot>)
-            .toJSON()
+        .create(<WhenNot screen_xs or screen_lg>medium screen</WhenNot>)
+        .toJSON()
       expect(tree).toBe('medium screen')
     })
 
     it('return null when not match', () => {
       const tree = renderer
-            .create(<WhenNot screen_xs or screen_md>large screen</WhenNot>)
-            .toJSON()
+        .create(<WhenNot screen_xs or screen_md>large screen</WhenNot>)
+        .toJSON()
       expect(tree).toBe(null)
     })
   })
