9afe9d756a80a4cee2ad0306a0e01625b5c95cb2
Sho Hashimoto <shokai@users.noreply.github.com>
2018-03-28 11:00:26 +0900

    Update npms for test (#8)
    * update browserify, jest, standard
    
    * follow standard

--- test/when.js
+++ test/when.js
@@ -26,8 +26,8 @@ describe('When component', () => {
   describe('simple condition case', () => {
     it('return children when match', () => {
       const tree = renderer
-            .create(<When login_user><a href='./profile'>profile page</a></When>)
-            .toJSON()
+        .create(<When login_user><a href='./profile'>profile page</a></When>)
+        .toJSON()
       expect(tree.type).toBe('a')
       expect(tree.props.href).toBe('./profile')
     })
@@ -35,8 +35,8 @@ describe('When component', () => {
     it('return null when not match', () => {
       When.case('admin_user', () => false)
       const tree = renderer
-            .create(<When admin_user><a href='./settings'>settings page</a></When>)
-            .toJSON()
+        .create(<When admin_user><a href='./settings'>settings page</a></When>)
+        .toJSON()
       expect(tree).toBe(null)
     })
   })
@@ -48,16 +48,16 @@ describe('When component', () => {
       When.case('screen_md', () => windowWidth >= 768 && windowWidth < 992)
       When.case('screen_lg', () => windowWidth >= 992)
       const tree = renderer
-            .create(<When login_user and screen_md><p>hello login user</p></When>)
-            .toJSON()
+        .create(<When login_user and screen_md><p>hello login user</p></When>)
+        .toJSON()
       expect(tree.type).toBe('p')
       expect(tree.children).toEqual(['hello login user'])
     })
 
     it('return null when not match', () => {
       const tree = renderer
-            .create(<When login_user and admin_user><a href='./settings'>settings page</a></When>)
-            .toJSON()
+        .create(<When login_user and admin_user><a href='./settings'>settings page</a></When>)
+        .toJSON()
       expect(tree).toBe(null)
     })
   })
@@ -65,15 +65,15 @@ describe('When component', () => {
   describe('"or" conditions', () => {
     it('return children when match', () => {
       const tree = renderer
-            .create(<When screen_md or screen_lg>large screen</When>)
-            .toJSON()
+        .create(<When screen_md or screen_lg>large screen</When>)
+        .toJSON()
       expect(tree).toBe('large screen')
     })
 
     it('return null when not match', () => {
       const tree = renderer
-            .create(<When screen_xs or screen_lg>not medium screen</When>)
-            .toJSON()
+        .create(<When screen_xs or screen_lg>not medium screen</When>)
+        .toJSON()
       expect(tree).toBe(null)
     })
   })
